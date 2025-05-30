464bf6025b12472b2ee020ff57770f898d78692e
Gabfonexe <bielfrancisco23@gmail.com>
2025-05-29 23:07:58 -0300

    Add requirements

--- Back/config.py
+++ Back/config.py
@@ -9,17 +9,19 @@ db_username = os.getenv("db_username")
 db_password = os.getenv("db_password")
 db_host = os.getenv("db_host")
 
+
 main_db_uri = (
-    "mysql://"
+    "postgresql+psycopg2://"
     + db_username
     + ":"
     + db_password
     + "@"
     + db_host
-    + ":3306/"
+    + ":5432/"
     + db_schema
-    + "?charset=utf8"
 )
 
 class Config:
     SQLALCHEMY_DATABASE_URI = main_db_uri
+
+
